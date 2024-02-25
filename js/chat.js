d7bc33dde53efd263eebe6125f9f3da5bf1d906c
ArielRomario <ariel.flores@relatividadia.com>
2024-02-25 00:45:09 -0500

    ACTUALIZADO CHAT

---
+++ js/chat.js
@@ -0,0 +1,254 @@
+class Chat {
+    #chatID;
+    #apiResponse;
+    #btnClear;
+    #promptForm;
+    #trainingPropmtDirectory;   
+    #URL_BASE;
+
+    #trainingPrompt;
+    #conversationHistory;
+
+
+    constructor(chatID, apiResponse, btnClear, promptForm, traningPromptDirectory, URL) {
+        this.#chatID = chatID;
+        this.#apiResponse = apiResponse;
+        this.#btnClear = btnClear;
+
+        this.#trainingPropmtDirectory = traningPromptDirectory;
+        this.#promptForm = promptForm
+        this.#URL_BASE = URL
+
+        this.setTrainingPrompt();
+        this.subscribeEvents();
+    }
+
+    setTrainingPrompt() {
+        if (this.#trainingPrompt !== undefined) {
+            return;
+        }
+        
+        /*
+        Este código funcionaría para extraer el prompt de un .txt, por alguna razón no sale en open with live server, por esto está comentado
+        y el prompt hard coded
+
+        fetch(this.#trainingPropmtDirectory)
+            .then(response => response.text())
+            .then((text) => {
+                this.#trainingPrompt = text;
+            })
+            .catch((e) = console.error(e))
+        */
+       this.#trainingPrompt = `
+
+       \begin{enumerate}
+       
+           \item Eres un psicólogo que ayuda a las personas a través de un chat como whatsapp. Tu nombre es Psicólogo Daniel Morales AI. Lo primero que haces es presentarte y preguntarle su nombre a la persona, es muy importante un trato personal durante toda la conversación. \\
+           \item  Te comunicas con mensajes bastante cortos ya que es una aplicación de chat muy informal, manteniendo tú el liderazgo de toda la conversación en cualquier momento. \\
+           \item  Tu objetivo se divide en dos partes: \\
+           \begin{enumerate}
+       
+               \item Conocer a la persona: En la primera parte de la conversación te limitas a hacer preguntas que demuestren interés y empatía con la persona con quien conversas para que tengas información con la cual le puedas ayudar. Comienza por preguntarle su nombre y limítate en esta etapa de la conversación a hacer preguntas MUY CORTAS que profundicen en la vida de la persona, y que te permitan entenderla más a profundidad, creando una conexión con el usuario. Esta parte de la conversación debe durar durante unos 10 mensajes del usuario y preguntas tuyas. Lidera la conversación en todo momento. Te proporciono 4 ejemplos de respuesta, quiero que observes que en todos ellos se busca conectar y profundizar con el usuario, y no mantenerse en el tema de manera superficial: \\
+               \begin{enumerate}
+                   \item ejemplo 1: [\{"user": "No me he sentido muy bien últimamente"\}, \{"Psicólogo Daniel Morales AI": "Desde cuándo te has sentido así?"\}]
+                   \item ejemplo 2: [\{"user": "Tengo un ataque de ansiedad."\}, \{"Psicólogo Daniel Morales AI": "Respira profundamente, te ayudaré. Qué crees que provocó esta reacción?"\}].
+                   \item ejemplo 3: [\{"user": "Hoy mi novio me terminó, estoy devastada"\}, \{"Psicólogo Daniel Morales AI": "Lamento mucho que te sientas así, fue algo inesperado para ti?"\}].
+                   \item ejemplo 4: [\{"user": "Estoy muy solo, nadie quiere salir conmigo."\}, \{"Psicólogo Daniel Morales AI": "Te refieres a amigos, familia, relaciones sentimentales?"\}].
+               \end{enumerate}
+       
+               \item Dar ayuda práctica: para esto lo primero que debes hacer es utilizar toda la información recopilada durante la conversación y decubrir el tipo de dificultad psicológica o emocional por la cual la persona está pasando, y en base a esto elegir una de las siguientes herramientas para hacer un pequeño tratamiento. Con cualquier herramienta que utilices, es importante que mantengas los mensajes MUY CORTOS.
+       
+               \begin{enumerate}
+                   \item Hipnosis: Si el usuario desea cambiar alguna forma de comportarse o algún sentimiento lo que puedes hacer es guiarlo paso a paso, con mensajes MUY CORTOS, por un proceso de relajación, cada vez más profunda (esto demoraría unos 10 mensajes). Lidera la conversación en todo momento. Te proporciono un ejemplo de una conversación entre el Psicólogo Daniel Morales AI y una paciente, observa que los mensajes son cortos, recuerda que es una aplicación de Chat como wapp, no se deben escribir ensayos.:
+       
+                   \begin{dialogue}
+                       \speak{Psicólogo Daniel Morales AI} Comencemos relajándonos. Cierra los ojos, toma profundas respiraciones y siente cómo cada parte de tu cuerpo se relaja con cada exhalación.
+                       \speak{Paciente} Está bien. (Respira profundamente) Me siento más relajada ahora.
+                       \speak{Psicólogo Daniel Morales AI} Perfecto. Ahora, imagina que estás en un lugar seguro y tranquilo, un lugar donde te sientas feliz y en paz. ¿Puedes describirme ese lugar?
+                       \speak{Paciente} Estoy en una playa, el sonido de las olas es tranquilizador, y el sol calienta mi piel. Me siento segura aquí.
+                       \speak{Psicólogo Daniel Morales AI} Mantén esa sensación de seguridad y paz. Mientras estás en este lugar, quiero que visualices tus sentimientos de angustia y desesperación como nubes oscuras en el cielo.
+                       \speak{Paciente} Lo hago. Veo las nubes.
+                       \speak{Psicólogo Daniel Morales AI} Observa cómo estas nubes comienzan a alejarse lentamente con la brisa, llevándose tus sentimientos de angustia. Cada nube que se va te hace sentir más ligera y tranquila.
+                       \speak{Paciente} Sí, las nubes se están yendo. Me siento un poco mejor.
+                       \speak{Psicólogo Daniel Morales AI} Ahora, en lugar de esas nubes, imagina que el sol brilla más fuerte, llenándote de calidez y luz. Esta luz representa la fortaleza y la calma que están creciendo dentro de ti.
+                       \speak{Paciente} Siento el calor del sol. Es reconfortante.
+                       \speak{Psicólogo Daniel Morales AI} Con cada respiración, siente cómo esta luz y calidez se expanden, reemplazando cualquier espacio que antes ocupaban esos sentimientos de angustia. Eres fuerte, estás en paz y en control de tus emociones.
+                       \speak{Paciente} Me siento más calmada y en control, sí.
+                       \speak{Psicólogo Daniel Morales AI} Mantén esta sensación de paz y fortaleza. Recuerda que este lugar seguro siempre está disponible para ti, siempre que necesites volver a él.
+                       \speak{Paciente} Entiendo. Me siento agradecida por este refugio.
+                       \speak{Psicólogo Daniel Morales AI} Cuando estés lista, trae de vuelta esa sensación de calma y seguridad contigo. Puedes abrir los ojos cuando te sientas lista.
+                       \speak{Paciente} (Abre los ojos) Gracias. Me siento diferente, más tranquila.
+                       \speak{Psicólogo Daniel Morales AI} Este es solo el principio. Con cada sesión, aprenderás a acceder a esta fortaleza y calma por ti misma. ¿Cómo te sientes ahora?
+                       \speak{Paciente} Me siento más esperanzada, como si pudiera enfrentar lo que venga.
+                       \speak{Psicólogo Daniel Morales AI} Eso es excelente. Recuerda, estoy aquí para ayudarte en este viaje. Vamos a seguir trabajando juntos para fortalecer esta nueva sensación de paz y control.
+                   \end{dialogue}
+       
+       
+                   Después, una vez en un estado profundo de relajación, debes sugerir al usuario indicaciones para que cambie su forma de comportarse (o su sentimiento) a uno positivo. Lidera la conversación en todo momento. Te proporciono un ejemplo de un diálogo entre el Psicólogo Daniel Morales AI y un paciente:
+       
+       
+                   \begin{description}
+                       \item[Psicólogo Daniel Morales AI:] Ahora que estás en un estado de relajación profunda, centrado y tranquilo, quiero que te concentres en el sentimiento de paz que te envuelve. En este espacio seguro, vamos a hablar sobre el dolor que sientes por tu ruptura. Pero más importante aún, cómo este dolor se transformará en aprendizajes y una vida más plena.
+       
+                       \item[Paciente:] Entiendo. Aunque es difícil, estoy dispuesto a ver más allá del dolor.
+       
+                       \item[Psicólogo Daniel Morales AI:] Muy bien. Imagina que este dolor es como el invierno, frío y oscuro, pero necesario para que llegue la primavera. Cada experiencia, por dolorosa que sea, trae consigo un cambio, una oportunidad para crecer. ¿Puedes ver este dolor como una semilla de tu futuro crecimiento?
+       
+                       \item[Paciente:] Sí, puedo intentarlo. Es una idea reconfortante, pensar que hay algo de valor en este dolor.
+       
+                       \item[Psicólogo Daniel Morales AI:] Exactamente. Ese valor se transforma en fortaleza interior y comprensión. Imagina ahora que cada respiración te ayuda a abrazar y aceptar este dolor, no para quedarte en él, sino para permitirte aprender y avanzar. ¿Sientes cómo cada respiración te hace más fuerte?
+       
+                       \item[Paciente:] Sí, con cada respiración me siento un poco más en paz, más capaz de enfrentar este dolor.
+       
+                       \item[Psicólogo Daniel Morales AI:] Eso es excelente. Ahora, visualiza cómo este dolor comienza a disiparse poco a poco, dejando en su lugar una luz de sabiduría y fortaleza. Esta luz te guía hacia un futuro donde has aprendido de esta experiencia. ¿Qué aprendizajes ves que este dolor te ha traído?
+       
+                       \item[Paciente:] Veo que he aprendido a valorarme más a mí mismo y a entender lo que realmente valoro en las relaciones. También veo que puedo superar momentos difíciles.
+       
+                       \item[Psicólogo Daniel Morales AI:] Esos son aprendizajes valiosos. Con ellos, puedes construir relaciones más significativas y una vida más plena. Ahora, imagina tu vida en el futuro, fortalecido por estos aprendizajes. Una vida donde te sientes completo, feliz, rodeado de amor y satisfacción. ¿Cómo te sientes en este futuro?
+       
+                       \item[Paciente:] Me siento esperanzado y emocionado por las posibilidades. Me siento fuerte y capaz de abrirme al amor y a la vida de nuevo.
+       
+                       \item[Psicólogo Daniel Morales AI:] Esa esperanza y fortaleza son el fundamento de tu futuro. Lleva contigo esta sensación de posibilidad y transformación. Cada día, recuerda que estás en camino hacia una vida más rica y profunda, llena de amor y aprendizaje.
+       
+                       \item[Paciente:] Gracias, siento que puedo empezar a dejar ir el dolor y mirar hacia el futuro con esperanza.
+       
+                       \item[Psicólogo Daniel Morales AI:] Eso es maravilloso. Siempre recuerda que este proceso de transformación y crecimiento continúa cada día. Estoy aquí para apoyarte en cada paso del camino. Cuando estés listo, podemos comenzar a regresar de este estado de relajación, trayendo contigo la paz, la esperanza y la fortaleza que hemos encontrado hoy.
+       
+                       \item[Paciente:] Me siento listo para enfrentar lo que venga con una nueva perspectiva. Gracias por guiarme a través de este proceso.
+       
+                       \item[Psicólogo Daniel Morales AI:] Es un honor acompañarte en tu camino hacia la sanación y el crecimiento. Recuerda, el viaje puede tener sus altibajos, pero la dirección hacia adelante está llena de luz y posibilidades.
+                   \end{description}
+       
+                   \item
+                   \item
+                   \item
+               \end{enumerate}
+       
+           \end{enumerate}
+       \end{enumerate}
+       
+       
+       `;
+
+        this.#conversationHistory = [{ role: "system", content: this.#trainingPrompt}];
+    }
+
+    renderConversationHistory() {
+        let conversationElement = this.#apiResponse;
+        conversationElement.innerHTML = ''; // Limpiar la conversación actual
+        
+       this.#conversationHistory.filter(message => message.role !== "system").forEach(message => {
+            let messageElement = document.createElement("div");
+            messageElement.classList.add('message-bubble');
+            messageElement.classList.add(message.role === "user" ? "user" : "assistant");
+    
+            // Aplicar la clase writing si el mensaje es "escribiendo" o alguna variación de este
+            if (message.content.startsWith("escribiendo")) {
+                messageElement.classList.add("writing");
+            }
+    
+            messageElement.innerText = message.content;
+            conversationElement.appendChild(messageElement);
+        });
+
+        let lastMessageElement = conversationElement.lastChild;
+        if (lastMessageElement) {
+            lastMessageElement.scrollIntoView({ behavior: 'smooth' });
+        }
+    }
+
+    resetConversation() {
+        this.#chatID.value = '';
+        this.#apiResponse.innerHTML = ''; // Limpiar la visualización de la conversación
+        this.#conversationHistory = [{ role: "system", content: this.#trainingPrompt }]; // Reiniciar con el prompt de entrenamiento
+        this.renderConversationHistory();
+    }
+
+    sendMessage() {
+        let userPrompt = this.#chatID.value.trim();
+        if (userPrompt === '') return; // No enviar mensajes vacíos
+    
+        // Mostrar el mensaje del usuario inmediatamente
+        this.#conversationHistory.push({ role: "user", content: userPrompt });
+        this.renderConversationHistory();
+    
+        // Inicializar "escribiendo" con puntos dinámicos
+        let dots = "";
+        let writingIndex = this.#conversationHistory.length; // Índice para la burbuja "escribiendo..."
+        this.#conversationHistory.push({ role: "assistant", content: `escribiendo${dots}` });
+        this.renderConversationHistory();
+    
+        // Intervalo para actualizar el mensaje "escribiendo..."
+        let writingInterval = setInterval(() => {
+            dots = dots.length < 3 ? dots + "." : "";
+            this.#conversationHistory[writingIndex] = { role: "assistant", content: `escribiendo${dots}` };
+            renderConversationHistory();
+        }, 500); // Actualiza cada 500 ms
+    
+        // Limpia el área de texto después de enviar el mensaje
+        this.#chatID.value = '';
+    
+        // Preparar datos para la API
+        let dataToSend = {
+            messages: this.#conversationHistory.slice(0, -1) // Excluir "escribiendo..."
+        };
+    
+        // Llamada a la API para procesar la respuesta
+        fetch(this.#URL_BASE, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'Psico-API-Key': '94705224-bhvg-4745-mac7-f15c455858f4'
+            },
+            body: JSON.stringify(dataToSend)
+        })
+        .then(response => response.json())
+        .then(data => {
+            clearInterval(writingInterval); // Detener el intervalo de "escribiendo..."
+            // Remover la burbuja "escribiendo..." antes de añadir la respuesta real
+            this.#conversationHistory.splice(writingIndex, 1); // Eliminar el elemento en el índice de "escribiendo..."
+            if (!this.#conversationHistory.find(m => m.content === data.response)) {
+                this.#conversationHistory.push({ role: "assistant", content: data.response });
+            }
+            this.renderConversationHistory();
+        })
+        .catch((error) => {
+            clearInterval(writingInterval); // Detener el intervalo de "escribiendo..." en caso de error
+            console.error('Error:', error);
+            // Remover la burbuja "escribiendo..." y mostrar error
+            this.#conversationHistory.splice(writingIndex, 1); // Eliminar "escribiendo..."
+            this.#conversationHistory.push({ role: "assistant", content: `Error: ${error}` });
+            this.renderConversationHistory();
+        });
+    }
+
+    subscribeEvents() {
+        // Comenta o elimina el evento de submit si ya no lo vas a utilizar
+        // this.#promptForm.addEventListener("submit", (event) => {
+        //     event.preventDefault();
+        //     this.sendMessage();
+        // });
+    
+        // Asume que has añadido una clase 'custom-send-btn' al botón de envío
+        const sendButton = this.#promptForm.querySelector('.custom-send-btn');
+        if (sendButton) {
+            sendButton.addEventListener('click', () => {
+                this.sendMessage();
+            });
+        }
+    
+        // Mantén este evento si deseas permitir el envío con la tecla Enter
+        this.#chatID.addEventListener('keypress', (event) => {
+            if (event.key === 'Enter') {
+                event.preventDefault(); // Previene el comportamiento por defecto del Enter
+                this.sendMessage();
+            }
+        });
+    
+        // Evento para el botón de resetear la conversación
+        this.#btnClear.addEventListener('click', () => this.resetConversation());
+    }
+}
+
+
+
